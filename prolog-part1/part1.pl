6d544935870cf28526a36f2edf002a7b8f5321dd
Rowan Walshe <rowan@walshe.io>
2021-03-24 09:03:53 +0000

    Made solutions a little less repetitive (even if they're now a bit longer)

--- prolog-part1/part1.pl
+++ prolog-part1/part1.pl
@@ -31,19 +31,31 @@ parse_instructions([HI|TI],[HV|TV],PV):-
 
 %% Gets the next value, given a list of instructions, and the starting position
 get_value([],V,V).
-get_value([H|T],V1,V):- H is 85, V1 is 1, get_value(T,1,V),!.
-get_value([H|T],V1,V):- H is 85, V1 is 2, get_value(T,2,V),!.
-get_value([H|T],V1,V):- H is 85, V1 is 3, get_value(T,3,V),!.
-get_value([H|T],V1,V):- H is 76, V1 is 1, get_value(T,1,V),!.
-get_value([H|T],V1,V):- H is 76, V1 is 4, get_value(T,4,V),!.
-get_value([H|T],V1,V):- H is 76, V1 is 7, get_value(T,7,V),!.
-get_value([H|T],V1,V):- H is 82, V1 is 3, get_value(T,3,V),!.
-get_value([H|T],V1,V):- H is 82, V1 is 6, get_value(T,6,V),!.
-get_value([H|T],V1,V):- H is 82, V1 is 9, get_value(T,9,V),!.
-get_value([H|T],V1,V):- H is 68, V1 is 7, get_value(T,7,V),!.
-get_value([H|T],V1,V):- H is 68, V1 is 8, get_value(T,8,V),!.
-get_value([H|T],V1,V):- H is 68, V1 is 9, get_value(T,9,V),!.
-get_value([H|T],V1,V):- H is 85, V2 is V1 - 3, get_value(T,V2,V).
-get_value([H|T],V1,V):- H is 76, V2 is V1 - 1, get_value(T,V2,V).
-get_value([H|T],V1,V):- H is 82, V2 is V1 + 1, get_value(T,V2,V).
-get_value([H|T],V1,V):- H is 68, V2 is V1 + 3, get_value(T,V2,V).
+get_value([H|T],V1,V):- up(H), top_key(V1), get_value(T,V1,V),!.
+get_value([H|T],V1,V):- left(H), left_key(V1), get_value(T,V1,V),!.
+get_value([H|T],V1,V):- right(H), right_key(V1), get_value(T,V1,V),!.
+get_value([H|T],V1,V):- down(H), bottom_key(V1), get_value(T,V1,V),!.
+get_value([H|T],V1,V):- up(H), V2 is V1 - 3, get_value(T,V2,V).
+get_value([H|T],V1,V):- left(H), V2 is V1 - 1, get_value(T,V2,V).
+get_value([H|T],V1,V):- right(H), V2 is V1 + 1, get_value(T,V2,V).
+get_value([H|T],V1,V):- down(H), V2 is V1 + 3, get_value(T,V2,V).
+
+%% Definition of instructions
+up(85).
+left(76).
+right(82).
+down(68).
+
+%% Definition of edge keys on the keypad
+top_key(1).
+top_key(2).
+top_key(3).
+left_key(1).
+left_key(4).
+left_key(7).
+right_key(3).
+right_key(6).
+right_key(9).
+bottom_key(7).
+bottom_key(8).
+bottom_key(9).
